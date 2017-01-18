6053c0578e11f30153506ce1c4db6700e7388e97
Ning Ma <n.ma@sheffield.ac.uk>
2017-01-18 22:28:56 +0100

    vol

--- twoears_demo_idloc/setupScenes.m
+++ twoears_demo_idloc/setupScenes.m
@@ -1,8 +1,12 @@
 function [sourceSets, sourceVolumes] = setupScenes
 
+% 'alarm'   'baby'  'fire'  'femaleSpeech'   'maleSpeech' 
+% 31        17      16      21               24
+
+
 sourceSets{1} = {'femaleSpeech'};
-sourceVolumes{1} = [1];
+sourceVolumes{1} = [21];
 sourceSets{2} = {'alarm'};
-sourceVolumes{2} = [1];
+sourceVolumes{2} = [31];
 sourceSets{3} = {'alarm', 'femaleSpeech'};
-sourceVolumes{3} = [1 1];
+sourceVolumes{3} = [31 21];
